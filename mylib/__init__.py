bcd764a07509c495cc40e0f183f56e89b8fe9850
Amir Shkedy <amirshkedy@shkedy.com>
2020-08-16 17:22:45 +0000

    last modification before docs should work

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,5 +1,7 @@
 def hello():
+    """first function check"""
     return 'Hello'
 
 def heyyou():
+    """second function check"""
     return 'Hey You'
--- mylib/another.py
+++ mylib/another.py
@@ -1,2 +1,3 @@
 def showme():
+    """This is just a test to see if its working 3rd function"""
     return "***^***"
